350a8127bbe381f032c2b1687c2dbe89c4ce0778
Ajay Mehta <dev.ajaymehta@gmail.com>
2017-06-29 06:47:42 +0000

    MainActivity.java edited online with Bitbucket

--- app/src/main/java/ajaymehta/arrayofobjects/MainActivity.java
+++ app/src/main/java/ajaymehta/arrayofobjects/MainActivity.java
@@ -31,3 +31,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
+/*
+There are some inconveniences in this mechanism
+1. we cant store differnt classo bject into the same array...array only store one data type of elements...see above array..we created just one type (student array)
+2. adding objects at end is easy..but inserting and deleting the elements in the middle of array is difficult..we have to re-arrange all the elements of array.
+3.retriving the elemnts from an array is easy but after reteriving the elements,if we want to process them, there are no methods available ex getFirst (to get first element ) , index0f (to find index of particular element)..we have to add all things manually.
+so we needed other better mechanism to store group of object.. so we got alternative is to store group of objects into an object....we can use class object as an array..called 
+collection object or container object. 
+ex Set<T> , List<T> , Queue, Map..  */
